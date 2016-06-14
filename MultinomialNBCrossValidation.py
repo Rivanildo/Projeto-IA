fe4e6bfd82adc4d4ac944a63485c8cbd7e91fb69
deyvison <deyvison.melo@dce.ufpb.br>
2016-06-13 22:46:53 -0300

    cross validation implementado

--- MultinomialNBCrossValidation.py
+++ MultinomialNBCrossValidation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.cross_validation import cross_val_score
@@ -18,17 +19,30 @@ x_train = vectorizer.fit_transform(train.Comment)
 x_test = vectorizer.transform(test.Comment)
 
 # classificador do pacote naive bayes
-classificador = MultinomialNB()
+classificador = MultinomialNB(alpha=1.0)
 
+alphas = np.logspace(-4, -.5, 30)
+
+
+for alpha in alphas:
+    classificador.alpha = alpha
+    this_scores = cross_val_score(classificador, x_train, train.Insult, n_jobs=1)
+
+    model = classificador.fit(x_train, list(train.Insult))
+
+    predictions = model.predict_proba(x_test)[:,1]
+
+    print predictions
+    print "-----"
 
 # Validacao cruzada
-scores = cross_val_score(classificador, x_train, train.Insult, cv = 10, scoring="roc_auc")
+# scores = cross_val_score(classificador, x_train, train.Insult, cv=None, scoring="roc_auc")
 
 # Treinar o naive bayes
-model = classificador.fit(x_train, list(train.Insult))
+# model = classificador.fit(x_train, list(train.Insult))
 
 #Classifica os tests
-predictions = model.predict_proba(x_test)[:,1]
+# predictions = model.predict_proba(x_test)[:,1]
 
 
 # gravando os resultados no arquivo
